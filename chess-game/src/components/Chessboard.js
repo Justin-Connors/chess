c1312383f00d26853870bf213454af9e3a4d8062
justin connors <justinconnors@hotmail.ca>
2023-09-04 16:16:44 -0300

    chessboard created

---
+++ chess-game/src/components/Chessboard.js
@@ -0,0 +1,64 @@
+import React from "react";
+import "./Chessboard.css";
+
+function Chessboard({ playerColor = "white" }) {
+  // 8 x 8 chessboard
+  const rows = [];
+  // row labels
+  let rowLabels = ["8", "7", "6", "5", "4", "3", "2", "1"];
+  // column labels
+  let columnLabels = ["A", "B", "C", "D", "E", "F", "G", "H"];
+
+  if (playerColor === "black") {
+    rowLabels = rowLabels.reverse();
+    columnLabels = columnLabels.reverse();
+  }
+
+  for (let i = 0; i < 8; i++) {
+    const columns = [];
+    for (let j = 0; j < 8; j++) {
+      const squareColor = (i + j) % 2 === 0 ? "white" : "black";
+      columns.push(
+        <div
+          key={`${i}-${j}`}
+          className={`square ${squareColor}`}
+          onClick={() => handleSquareClick(i, j)}
+        ></div>
+      );
+    }
+    rows.push(
+      <React.Fragment key={i}>
+        <div className="row-label">{rowLabels[i]}</div>
+        {columns}
+        <div className="row-label">{rowLabels[i]}</div>
+      </React.Fragment>
+    );
+  }
+
+  //handle square click
+  const handleSquareClick = (row, column) => {
+    console.log(`Clicked on square: ${row}, ${column}`);
+    // need to add piece movement logic
+  };
+
+  return (
+    <div className="chessboard">
+      <div></div>
+      {columnLabels.map((label, index) => (
+        <div key={index} className="column-label">
+          {label}
+        </div>
+      ))}
+      <div></div>
+      {rows}
+      <div></div>
+      {columnLabels.map((label, index) => (
+        <div key={index} className="column-label">
+          {label}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Chessboard;
